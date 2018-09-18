816a4930a4523ce27628af600dbc24822461c2b2
Boris Hasikov <laapl@yandex.ru>
2018-09-18 23:15:56 +0300

    Фикс путей для одновременной работы под ubuntu и windows

--- build/Routes/Desktop/index.js
+++ build/Routes/Desktop/index.js
@@ -1,9 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const path_1 = __importDefault(require("path"));
 const router = express_1.Router();
 router.get('*', (req, res, next) => {
-    res.sendFile(process.cwd() + "\\static\\desktop\\index.html");
+    res.sendFile(path_1.default.resolve(process.cwd(), "/static/desktop/index.html"));
 });
 exports.default = router;
 //# sourceMappingURL=index.js.map
--- server/Routes/Desktop/index.ts
+++ server/Routes/Desktop/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import path from "path";
 
 const router = Router();
 
 router.get('*', (req, res, next) => {
     // console.log(process.cwd())
-    res.sendFile(process.cwd() + "\\static\\desktop\\index.html");
+    res.sendFile(path.resolve(process.cwd(), "/static/desktop/index.html"));
 });
 
 export default router;
