09518e54ae5fe06d71077001f0ebe084b9aa929c
Nikita Bersenev <nikita.bersenev@tele2.ru>
2018-09-21 02:54:10 +0800

    Тестовый коммит

--- server/Routes/Desktop/index.ts
+++ server/Routes/Desktop/index.ts
@@ -3,6 +3,8 @@ import path from "path";
 
 const router = Router();
 
+// Тестовый коммит
+
 router.get('*', (req, res, next) => {
     // console.log(process.cwd())
     res.sendFile(path.resolve(process.cwd(), "static/desktop/index.html"));
