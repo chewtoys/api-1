e9ae16714cc6df415f08725f3060f596ed440709
Boris Hasikov <laapl@yandex.ru>
2018-10-02 02:09:37 +0300

    Изменения минорные на сервере

--- server/API/index.ts
+++ server/API/index.ts
@@ -1,27 +1,29 @@
 /**
- * Класс для работы с API 
+ * Класс для работы с API
  * @author Nikita Bersenev
  */
 
-interface paramsCall {
-  classname: string,
-  method: string,
-  params?: any
+import Products from "./Products";
+
+// interface paramsCall {
+//   classname: string,
+//   method: string,
+//   params?: any
+// }
+
+export default class API {
+    [propName: string]: any;
+    Products: Products;
+
+    constructor() {
+        this.Products = new Products();
+    }
+
+    // public call (options: paramsCall) {
+    //   this.class = new [options.classname]
+    //   // this.class = require('./' + options.classname)
+    //   return this.class[options.method](options.params)
+    // }
 }
 
-class API {
-
-  class: any
-
-  constructor() {
-    this.class = {}
-  }
-
-  public call (options: paramsCall) {
-    this.class = require('./' + options.classname)
-    return this.class[options.method](options.params)
-  }
-
-}
-
-export default new API()
+// export default new API()
