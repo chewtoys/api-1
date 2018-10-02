4a7d9f2a1d00349e6dbb4f4dc1b8c097a026ab6d
Nikita Bersenev <nikita.bersenev@tele2.ru>
2018-10-03 02:29:44 +0800

    Небольшие правки

--- server/API/index.ts
+++ server/API/index.ts
@@ -5,25 +5,11 @@
 
 import Products from "./Products";
 
-// interface paramsCall {
-//   classname: string,
-//   method: string,
-//   params?: any
-// }
-
 export default class API {
     [propName: string]: any;
     Products: Products;
 
     constructor() {
-        this.Products = new Products();
+        this.Products = new Products;
     }
-
-    // public call (options: paramsCall) {
-    //   this.class = new [options.classname]
-    //   // this.class = require('./' + options.classname)
-    //   return this.class[options.method](options.params)
-    // }
 }
-
-// export default new API()
--- server/Routes/Desktop/index.ts
+++ server/Routes/Desktop/index.ts
@@ -1,30 +1,29 @@
-import { Router } from "express"
-import path from "path"
-import API from '../../API'
+import { Router } from "express";
+import path from "path";
+import API from '../../API';
 
-const router = Router()
+const router = Router();
 
 router.all('/api/:classname/:method', async (req, res, next) => {
-  const classname = req.params.classname[0].toUpperCase() + req.params.classname.slice(1)
-  const method = req.params.method
-  const params = req.query
-  console.log(process)
+  const classname = req.params.classname[0].toUpperCase() + req.params.classname.slice(1);
+  const method = req.params.method;
+  const params = req.query;
+  // console.log(process)
 
   try {
-    // res.json(await new API()[classname][method](params));
-    res.json(await new API().Products.getItems());
+    res.json(await new API()[classname][method](params));
   } catch (e) {
     res.json({
       result: false,
       error_text: e.message,
       error_stack: e.stack
-    })
+    });
   }
-})
+});
 
 router.get('*', (req, res, next) => {
     // console.log(process.cwd())
     res.sendFile(path.resolve(process.cwd(), "static/desktop/index.html"));
-})
+});
 
-export default router
+export default router;
