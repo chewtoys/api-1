16bbeec4af065cdac7b5d9cf66a99c4b73770503
Yury Savin <savinyurii@gmail.com>
2018-10-03 01:19:26 +0800

    remove unused

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,20 +25,9 @@ module.exports = {
             host: "node3.ortant.ru",
             ref: "origin/master",
             repo: "https://github.com/laapl/kfc.git",
-            path: "/home/projects/delivery/kfc/",
+            path: "/home/projects/savin/delivery/kfc/",
             "post-deploy":
                 "yarn install && tsc && node client/desktop/scripts/start.js && pm2 reload ecosystem.config.js --env production"
         }
-        // dev: {
-        //     user: 'node',
-        //     host: '212.83.163.1',
-        //     ref: 'origin/master',
-        //     repo: 'git@github.com:repo.git',
-        //     path: '/var/www/development',
-        //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
-        //     env: {
-        //         NODE_ENV: 'dev'
-        //     }
-        // }
     }
 };
