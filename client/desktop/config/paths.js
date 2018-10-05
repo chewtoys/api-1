5d097595a4185905a5ad09b6d2d63ca8f10e1079
Boris Hasikov <laapl@yandex.ru>
2018-10-05 21:35:00 +0300

    Merge none_modules client and server 🐟

--- client/desktop/config/paths.js
+++ client/desktop/config/paths.js
@@ -1,29 +1,28 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const fs = require('fs');
-const url = require('url');
+const path = require("path");
+const fs = require("fs");
+const url = require("url");
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebook/create-react-app/issues/637
 const appDirectory = fs.realpathSync(process.cwd());
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
 const envPublicUrl = process.env.PUBLIC_URL;
 
 function ensureSlash(inputPath, needsSlash) {
-  const hasSlash = inputPath.endsWith('/');
-  if (hasSlash && !needsSlash) {
-    return inputPath.substr(0, inputPath.length - 1);
-  } else if (!hasSlash && needsSlash) {
-    return `${inputPath}/`;
-  } else {
-    return inputPath;
-  }
+    const hasSlash = inputPath.endsWith("/");
+    if (hasSlash && !needsSlash) {
+        return inputPath.substr(0, inputPath.length - 1);
+    } else if (!hasSlash && needsSlash) {
+        return `${inputPath}/`;
+    } else {
+        return inputPath;
+    }
 }
 
-const getPublicUrl = appPackageJson =>
-  envPublicUrl || require(appPackageJson).homepage;
+const getPublicUrl = (appPackageJson) => envPublicUrl || require(appPackageJson).homepage;
 
 // We use `PUBLIC_URL` environment variable or "homepage" field to infer
 // "public path" at which the app is served.
@@ -32,26 +31,25 @@ const getPublicUrl = appPackageJson =>
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
 function getServedPath(appPackageJson) {
-  const publicUrl = getPublicUrl(appPackageJson);
-  const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
-  return ensureSlash(servedUrl, true);
+    const publicUrl = getPublicUrl(appPackageJson);
+    const servedUrl = envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : "/");
+    return ensureSlash(servedUrl, true);
 }
 
 // config after eject: we're in ./config/
 module.exports = {
-  dotenv: resolveApp(".env"),
-  appPath: resolveApp("."),
-  appBuild: resolveApp("../../static/desktop"),
-  appPublic: resolveApp("public"),
-  appHtml: resolveApp("public/index.html"),
-  appIndexJs: resolveApp("src/index.js"),
-  appPackageJson: resolveApp("package.json"),
-  appSrc: resolveApp("src"),
-  yarnLockFile: resolveApp("yarn.lock"),
-  testsSetup: resolveApp("src/setupTests.js"),
-  proxySetup: resolveApp("src/setupProxy.js"),
-  appNodeModules: resolveApp("node_modules"),
-  publicUrl: getPublicUrl(resolveApp("package.json")),
-  servedPath: getServedPath(resolveApp("package.json"))
+    dotenv: resolveApp("./client/desktop/config/env"),
+    appPath: resolveApp("./client/desktop"),
+    appBuild: resolveApp("./static/desktop"),
+    appPublic: resolveApp("./client/desktop/public"),
+    appHtml: resolveApp("./client/desktop/public/index.html"),
+    appIndexJs: resolveApp("./client/desktop/src/index.js"),
+    appPackageJson: resolveApp("package.json"),
+    appSrc: resolveApp("./client/desktop/src"),
+    yarnLockFile: resolveApp("yarn.lock"),
+    testsSetup: resolveApp("./client/desktop/src/setupTests.js"),
+    proxySetup: resolveApp("./client/desktop/src/setupProxy.js"),
+    appNodeModules: resolveApp("node_modules"),
+    publicUrl: getPublicUrl(resolveApp("package.json")),
+    servedPath: getServedPath(resolveApp("package.json"))
 };
