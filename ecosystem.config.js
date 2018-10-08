1d0ccb6fd86ed222d04fa9b321e8553bb91f28a5
Yury Savin <savinyurii@gmail.com>
2018-10-08 14:40:12 +0300

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,15 +22,7 @@ module.exports = {
             ref: "origin/master",
             repo: "https://github.com/laapl/kfc.git",
             path: "/home/projects/savin/delivery/kfc/",
-            // Pre-setup command or path to a script on your local machine
-            // 'pre-setup': "ls -la",
-            // Post-setup commands or path to a script on the host machine
-            // eg: placing configurations in the shared dir etc
-            // 'post-setup': "ls -la",
-            // 'pre-deploy-local': "yarn build",
-            // 'pre-deploy': "",
-            "post-deploy":
-                "yarn install && tsc && yarn build:client && pm2 reload ecosystem.config.js --env production"
+            "post-deploy": "yarn install && tsc && yarn build:client && pm2 reload ecosystem.config.js --env production"
         }
     }
 };
