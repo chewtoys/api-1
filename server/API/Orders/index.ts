a08f69914fa56cc3ef17d4c17bbf19dc547fc710
Nikita Bersenev <bersenoff@bk.ru>
2018-10-10 02:33:09 +0800

    wrote method for changing state of orders

--- server/API/Orders/index.ts
+++ server/API/Orders/index.ts
@@ -1,5 +1,6 @@
 import Main from '../../Main';
 import Functions from '../../Main/Functions';
+import { parse } from 'url';
 
 export default class Orders extends Main {  
   functions: Functions;
@@ -29,16 +30,16 @@ export default class Orders extends Main {
    * @param {boolean} [paid] - order has been paid
    * @param {boolean} [debug=false] - debug mode
    */
-  public async create(params: any) {
+  public async create(query: any) {
 
-    params.session = '123';
+    query.session = '123';
 
     // check of required parameters
-    if (!params.session || !params.products || !params.address) {
-      if (params.debug) this.functions.paramsError();
+    if (!query.session || !query.products || !query.address) {
+      if (query.debug) this.functions.paramsError();
       else this.functions.unknownError();
-    } else if (!params.products.length) {
-      if (params.debug) throw new Error('Заказ пуст');
+    } else if (!query.products.length) {
+      if (query.debug) throw new Error('Заказ пуст');
       else this.functions.unknownError();
     }
 
@@ -48,11 +49,11 @@ export default class Orders extends Main {
 
     let idclient: number = 1;
     let paid = 0;
-    if (params.paid) paid = 1;
+    if (query.paid) paid = 1;
 
     const order_sql: string = `
       INSERT INTO ?? (idclient, idstate, address, comment, paid, start_datetime)
-      VALUES ('${idclient}', '${(!paid) ? '1' : '2'}', '${params.address}', ${(!params.comment) ? 'NULL' : "'" + params.comment + "'"}, '${paid}', CURRENT_TIMESTAMP)
+      VALUES ('${idclient}', '${(!paid) ? '1' : '2'}', '${query.address}', ${(!query.comment) ? 'NULL' : "'" + query.comment + "'"}, '${paid}', CURRENT_TIMESTAMP)
     `;
 
     const data: any = await this.Db.query(order_sql, [this.table.orders]);
@@ -65,7 +66,7 @@ export default class Orders extends Main {
     let data_sql: string = `INSERT INTO ?? (idorder, idproduct) VALUES `;
 
     let values: string[] = [];
-    params.products.forEach((idproduct: number) => {
+    query.products.forEach((idproduct: number) => {
       values.push(`('${idorder}', '${idproduct}')`);
     });
 
@@ -76,5 +77,35 @@ export default class Orders extends Main {
     this.response.result = true;
     
     return this.response;
-  };
+  }
+
+  /**
+   * @description order state change
+   * @param {number} idstate - state id
+   * @param {number} idorder - order id
+   */
+  public async setState(query: any) {
+    
+    // check of required parameters
+    if (!query.idorder || !query.idstate) {
+      if (query.debug) this.functions.paramsError();
+      else this.functions.unknownError();
+    } else if (isNaN(query.idorder) || isNaN(query.idstate)) {
+      if (query.debug) throw new Error('idorder and idstate must be a number!');
+      else this.functions.unknownError();
+    }
+
+    const idorder: number = parseInt(query.idorder);
+    const idstate: number = parseInt(query.idstate);
+
+    const sql: string = 'UPDATE ?? SET idstate = ? WHERE idorder = ?';
+    const params: any[] = [this.table.orders, idstate, idorder];
+
+    await this.Db.query(sql, params);
+
+    this.response.result = true;
+
+    return this.response;
+
+  }
 };
