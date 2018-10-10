52e3ed1f28821c241fd99081705045521b513010
Yury Savin <savinyurii@gmail.com>
2018-10-10 08:33:38 +0300

    release connection

--- server/Main/DateBase.ts
+++ server/Main/DateBase.ts
@@ -2,30 +2,35 @@
  * Класс для работы с БД
  * @author Nikita Bersenev
  */
-import mysql, { Pool } from 'mysql';
+import mysql, { Pool } from "mysql";
 
 export default class DB {
-  pool: Pool;
+    pool: Pool;
 
-  constructor () {
-    if(!process.env.MYSQL_USER) throw `You need to add to .env file: MYSQL_USER=''`;
-    if(!process.env.MYSQL_PASSWORD) throw `You need to add to .env file: MYSQL_PASSWORD=''`;
-    this.pool = mysql.createPool({
-      connectionLimit: 10,
-      host: "node3.ortant.ru",
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: "kfc",
-      dateStrings: true
-    });
-  }
+    constructor() {
+        if (!process.env.MYSQL_USER) throw `You need to add to .env file: MYSQL_USER=''`;
+        if (!process.env.MYSQL_PASSWORD) throw `You need to add to .env file: MYSQL_PASSWORD=''`;
+        this.pool = mysql.createPool({
+            connectionLimit: 10,
+            host: "node3.ortant.ru",
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: "kfc",
+            dateStrings: true
+        });
+    }
 
-  query(sql: string, params?: string[]) {
-    return new Promise((resolve, reject) => {
-      this.pool.query(sql, params, (err: mysql.MysqlError, res: any) => {
-        if (err) reject(err);
-        resolve(res);
-      });
-    });
-  };
-};
+    query(sql: string, params?: string[]) {
+        return new Promise((resolve, reject) => {
+            this.pool.getConnection((err, connection) => {
+                if (err) return reject(err);
+                connection.query(sql, params, (err: mysql.MysqlError, res: any) => {
+                    connection.release();
+                    if (err) return reject(err);
+                    return resolve(res);
+                });
+            });
+        });
+    }
+    
+}
