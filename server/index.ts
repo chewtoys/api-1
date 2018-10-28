fde0d7cb1419cf2bdb4f38197975f9797307f891
Nikita Bersenev <bersenoff@bk.ru>
2018-10-28 23:46:28 +0800

    fix

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ import connectRedis from 'connect-redis';
 import { desktopRoute, apiRoute, authRoute } from "./Routes";
 
 const RedisClient = redis.createClient({
-  host: 'node3.ortant.ru',
+  host: 'localhost',
   port: 6379,
   password: process.env.REDIS_PASSWORD
 });
