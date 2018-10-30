2c0d98bbbe10b433b358ca71249cd5b35ac7c5cc
Boris Hasikov <laapl@yandex.ru>
2018-10-30 21:21:49 +0300

    Clear package and store in localStorage.

--- client/desktop/src/components/BigCart/index.jsx
+++ client/desktop/src/components/BigCart/index.jsx
@@ -6,24 +6,24 @@ export default class BigCart extends React.Component {
     input = React.createRef();
 
     componentDidMount() {
-        window.ymaps.ready(() => {
-            const suggestView = new window.ymaps.SuggestView(this.input.current, {
-                results: 5,
-                offset: [20, 0],
-                boundedBy: [
-                    [52.288472, 104.239408],
-                    [52.221471, 104.292966],
-                    [52.248874, 104.365407],
-                    [52.302787, 104.344808]
-                ]
-            });
-        });
+        // window.ymaps.ready(() => {
+        //     const suggestView = new window.ymaps.SuggestView(this.input.current, {
+        //         results: 5,
+        //         offset: [0, 10],
+        //         boundedBy: [
+        //             [52.288472, 104.239408],
+        //             [52.221471, 104.292966],
+        //             [52.248874, 104.365407],
+        //             [52.302787, 104.344808]
+        //         ]
+        //     });
+        // });
     };
 
     render() {
         return (
             <div className="cart-big">
-                <input ref={this.input} id="suggest" type="text" />
+                <input style={{width: "500px"}} ref={this.input} id="suggest" type="text" />
             </div>
         )
     }
--- client/desktop/src/store.js
+++ client/desktop/src/store.js
@@ -1,10 +1,20 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { routerMiddleware, routerReducer } from "react-router-redux";
+import persistState from "redux-localstorage";
 import thunk from "redux-thunk";
+import moment from "moment";
 import createHistory from "history/createBrowserHistory";
 // Actions
 import { data, settings } from "./components/Main/actions";
-import logger from 'redux-logger';
+import logger from "redux-logger";
+
+if (typeof localStorage["checkDate"] === "undefined") {
+    localStorage["checkDate"] = moment().format("YYYY-MM-DD");
+} else if (localStorage["checkDate"] !== moment().format("YYYY-MM-DD")) {
+    localStorage["checkDate"] = moment().format("YYYY-MM-DD");
+} else {
+    localStorage.clear();
+}
 
 export const history = createHistory();
 
@@ -13,22 +23,26 @@ const redusers = combineReducers({
     data,
     settings
 });
+
 const initialState = {};
 const enhancers = [];
 let middleware = [thunk, routerMiddleware(history)];
 
 if (process.env.NODE_ENV === "development") {
-  const devToolsExtension = window.devToolsExtension;
-  middleware = middleware.concat(logger)
+    const devToolsExtension = window.devToolsExtension;
+    middleware = middleware.concat(logger);
 
-  if (typeof devToolsExtension === "function") {
-    enhancers.push(devToolsExtension());
-  }
-};
+    if (typeof devToolsExtension === "function") {
+        enhancers.push(devToolsExtension());
+    }
+}
 
 const composedEnhancers = compose(
-  applyMiddleware(...middleware),
-  ...enhancers
+    applyMiddleware(...middleware),
+    persistState(["data", "settings"], {
+        key: "root"
+    }),
+    ...enhancers
 );
 
 export default createStore(redusers, initialState, composedEnhancers);
