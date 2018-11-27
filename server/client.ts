97e08fc888f61c3e07eb87096a9db1bfaad9517a
Boris Hasikov <laapl@yandex.ru>
2018-11-27 11:48:35 +0300

    Mini fix for static.

--- server/client.ts
+++ server/client.ts
@@ -3,6 +3,10 @@ import path from "path";
 import http from "http";
 
 const App = express();
+
+App.use(express.static('static/desktop/'));
+App.use(express.static('upload/'));
+
 App.get("*", (req: express.Request, res: express.Response) => {
   res.sendFile(path.resolve(process.cwd(), "static/desktop/index.html"));
 });
