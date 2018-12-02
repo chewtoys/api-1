84f54fec7675388c6b9891ec09bf2b076bc9c5a2
Nikita Bersenev <bersenoff@bk.ru>
2018-12-03 01:49:07 +0800

    fix

--- server/Routes/Api/index.ts
+++ server/Routes/Api/index.ts
@@ -18,7 +18,7 @@ router.all('/api/orders/create', async (req, res, next) => {
 router.all("/api/:classname/:method", async (req, res, next) => {
     const classname = req.params.classname[0].toUpperCase() + req.params.classname.slice(1);
     const method = req.params.method;
-    const params = req.body;
+    const params = req.query;
 
     try {
         res.json(await new API()[classname][method](params));
