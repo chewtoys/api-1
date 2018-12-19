f396e00e5351ab8f0d5b32cfc5a82e65bdb56ce1
Boris Hasikov <laapl@yandex.ru>
2018-12-19 20:32:25 +0800

    Клиент мигрировал

--- client/desktop/src/components/Root/actions/index.js
+++
@@ -1,7 +0,0 @@
-import data from "./loadData";
-import settings from "./loadSettings";
-
-export {
-    data,
-    settings,
-}
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,32 +2,18 @@ module.exports = {
   apps: [
     {
       name: "server",
-      script: "./build/server.js",
-      watch: ["build", "static"],
+      script: "./build/index.js",
+      watch: ["build"],
       env: {
-        NODE_ENV: "development"
+        NODE_ENV: "development",
       },
       env_production: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
       },
       output: "./logs/out.log",
       error: "./logs/error.log",
-      log: "./logs/combined.outerr.log"
+      log: "./logs/combined.outerr.log",
     },
-    {
-      name: "client",
-      script: "./build/client.js",
-      watch: ["build", "static"],
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production"
-      },
-      output: "./logs/out.log",
-      error: "./logs/error.log",
-      log: "./logs/combined.outerr.log"
-    }
   ],
   deploy: {
     production: {
@@ -36,8 +22,7 @@ module.exports = {
       ref: "origin/master",
       repo: "https://github.com/laapl/api.git",
       path: "/home/laapl/delivery/",
-      "post-deploy":
-        "yarn && yarn tsc && yarn build:client && pm2 reload ecosystem.config.js --env production"
-    }
-  }
+      "post-deploy": "yarn && yarn tsc && pm2 reload ecosystem.config.js --env production",
+    },
+  },
 };
