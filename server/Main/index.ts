346a83e19bb221ce9ede56f6ce367d6d770dc66b
Boris Hasikov <laapl@yandex.ru>
2018-12-28 16:52:41 +0000

    К Logger'у

--- server/Main/index.ts
+++ server/Main/index.ts
@@ -1,9 +1,13 @@
 import Db from "./DataBase";
+import Logger from "./Logger";
 
 export default class Main {
   Db: Db;
+  Logger: Logger;
+
   constructor() {
     // // console.log("start");
     this.Db = new Db();
+    this.Logger = new Logger();
   }
 }
