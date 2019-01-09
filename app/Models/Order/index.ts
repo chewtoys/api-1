cb21bac47097e9fabe88070fc4a61390098da897
Boris Hasikov <laapl@yandex.ru>
2019-01-09 19:13:37 +0800

    API живи

--- app/Models/Order/index.ts
+++ app/Models/Order/index.ts
@@ -1,94 +1,98 @@
 export default (sequelize: any, Sequelize: any) => {
   // console.log(Sequelize);
 
-  const Order = sequelize.define("order", {
-    idorder: {
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      notEmpty: true,
-    },
-    idproject: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      notEmpty: true
-    },
-    idclient: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      notEmpty: true,
-    },
-    idcourier: {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-      notEmpty: true,
-    },
-    idstate: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      notEmpty: true,
-    },
-    lat: {
-      type: Sequelize.DECIMAL,
-      allowNull: false,
-      notEmpty: true,
-    },
-    lon: {
-      type: Sequelize.DECIMAL,
-      allowNull: false,
-      notEmpty: true,
-    },
-    address: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      notEmpty: true,
-    },
-    entrance: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      notEmpty: true,
-    },
-    apartment: {
-      type: Sequelize.STRING,
-      allowNull: true,
-      notEmpty: false,
-    },
-    intercom: {
-      type: Sequelize.STRING,
-      allowNull: true,
-      notEmpty: false,
-    },
-    comment: {
-      type: Sequelize.STRING,
-      allowNull: true,
-      notEmpty: false,
-    },
-    delivery_cost: {
-      type: Sequelize.FLOAT,
-      allowNull: false,
-      notEmpty: true
-    },
-    order_datetime: {
-      type: Sequelize.DATE,
-      allowNull: false,
-      notEmpty: true,
-    },
-    finish_datetime: {
-      type: Sequelize.DATE,
-      allowNull: true,
-      notEmpty: true,
-    },
-  }, {
-    indexes: [
-      {
-        fields: ['idproject']
+  const Order = sequelize.define(
+    "order",
+    {
+      order_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        notEmpty: true,
       },
-      {
-        fields: ['idclient']
-      }
-    ]
-  });
+      fk_project_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        notEmpty: true,
+      },
+      fk_user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        notEmpty: true,
+      },
+      fk_worker_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        notEmpty: true,
+      },
+      fk_status_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        notEmpty: true,
+      },
+      lat: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+        notEmpty: true,
+      },
+      lon: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+        notEmpty: true,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        notEmpty: true,
+      },
+      entrance: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        notEmpty: true,
+      },
+      apartment: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        notEmpty: false,
+      },
+      intercom: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        notEmpty: false,
+      },
+      comment: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        notEmpty: false,
+      },
+      delivery_cost: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        notEmpty: true,
+      },
+      order_datetime: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        notEmpty: true,
+      },
+      finish_datetime: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        notEmpty: true,
+      },
+    },
+    {
+      indexes: [
+        {
+          fields: ["fk_project_id"],
+        },
+        {
+          fields: ["fk_user_id"],
+        },
+      ],
+    }
+  );
 
   return Order;
 };
