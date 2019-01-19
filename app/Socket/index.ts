6355c4f4fd03e313811f5135b32e67ca6bb60d79
Nikita Bersenev <bersenoff@bk.ru>
2019-01-20 01:04:03 +0800

    Добавил глобальный обработчик сокетов

--- app/Socket/index.ts
+++ app/Socket/index.ts
@@ -1,10 +1,18 @@
 import io from "socket.io-client";
+import { Workers } from "../API";
 
 class Socket {
   bot: SocketIOClient.Socket;
 
   constructor() {
     this.bot = io.connect(process.env.BOT_SOCKET);
+  }
+
+  public eventHandler() {
+    // Изменение статуса курьера
+    this.bot.on("change_worker_status", async (data: any) => {
+      this.bot.emit("change_worker_status_res", await Workers.changeStatus(data));
+    });
   }
 }
 
